88c07d7833f3cf7262258a1f65ee47a9fd143d5a
Rorounifix <rorounifix@gmail.com>
2017-07-11 16:27:37 +0300

    Add comments

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import psutil
 from kivy.clock import Clock
 from re import findall
 
+#SET THE BOARDER SIZE
+
 Config.set('graphics','width','200')
 Config.set('graphics','height','100')
 Config.set('graphics','borderless','1')
@@ -17,87 +19,57 @@ Config.set('graphics','position', 'custom')
 
 Builder.load_string('''
 <Label>
-	font_size:'15dp'
+	font_size:'15dp'  					#----------CLASS FOR LABEL
 
-<RootWidget>
-	id:widget
+<RootWidget>						#------ROOT WIDGET
+	id:widget		
 	rows:5
 	padding:10
 	
-	ProgressBar:
+	ProgressBar:			#----- BAR FOR CPU
 		id:pb
 		value:0
-		
-	
-	Label:
+
+	Label:					#------ CPU USAGE TEXT
 		id:pb_label
 		text:'CPU 0%'
 	
-	ProgressBar:
+	ProgressBar:			#------- BAR FOR RAM
 		id:pb_ram
 		value:0
-		
 	
-	Label:
+	Label:					#------ RAM USAGE TEXT
 		id:pb_ram_label
 		text:'RAM 0%'
-	
-	# BoxLayout:
-		# orientation:'horizontal'
-		# spacing:10
-		# Button:
-			# id:start
-			# text:'Start'
-			# on_press:widget.start()
-		
-		# Button:
-			# id:stop
-			# text:'Stop'
-			# on_press:widget.stop()
-
 ''')
 
 
 class RootWidget(GridLayout):
 		
 	def __init__(self, **kwargs):
-		super(RootWidget, self).__init__(**kwargs)
-		self.pb_bar = self.ids['pb']
-		self.pb_ram = self.ids['pb_ram']
-		self.pb_label = self.ids['pb_label']
-		self.pb_ram_label = self.ids['pb_ram_label']
-		Clock.schedule_interval(self.pb, 1)
-	
-	
-	
-	def pb(self, dt):
+		super(RootWidget, self).__init__(**kwargs)	
+		self.pb_bar = self.ids['pb'] # 'pb' id for CPU ProgressBar
+		self.pb_ram = self.ids['pb_ram']  # 'pb_ram' id for RAM ProgressBar
+		self.pb_label = self.ids['pb_label']# CPU text label
+		self.pb_ram_label = self.ids['pb_ram_label']# RAM text label
+		Clock.schedule_interval(self.pb, 1)# callback every 1 secs
+
+	def pb(self, dt): 
 		
-		x = str(psutil.virtual_memory())
-		z = findall(r'\d{0,9}\d{0,9}\.\d{0,9}',x)
+		#i am using psutil to grab the usage for CPU and RAM	
+		x = str(psutil.virtual_memory()) 
+		z = findall(r'\d{0,9}\d{0,9}\.\d{0,9}',x) 
 		self.ram = float(z[0])
 		self.cpu = psutil.cpu_percent()
-		# print(self.ram,self.cpu)
-		
-		
+	
+		# i am passing the result to KV file
 		self.pb_bar.value = int(self.cpu)
 		self.pb_label.text = "CPU Usage {}%".format(self.cpu)
 		self.pb_ram.value = int(self.ram)
 		self.pb_ram_label.text = "RAM Usage {}%".format(self.ram)
 
-		
 		return 
 				
-		
-		
-	
-
-	
-	def stop(self):
-		pass
-		
-		
-		
-
 
 class MainApp(App):
 	
@@ -106,3 +78,4 @@ class MainApp(App):
 		
 if __name__ == "__main__":
 	MainApp().run()
+	
